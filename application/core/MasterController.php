2860833b33c1df90562dfaa7fd3f7b41a0907648
TANCode <peter.tanko@tancode.hu>
2020-01-13 23:04:48 +0100

    Initial commit

---
+++ application/core/MasterController.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require 'vendor/autoload.php';
+
+class MasterController extends CI_Controller {
+
+    protected $data;
+    protected $load_models = [];
+    protected $controller;
+    protected $js_custom_files = false;
+    protected $css_custom_files = false;
+
+    public function __construct() {
+		parent::__construct();
+		date_default_timezone_set('UTC');
+		$this->load_models = array_merge(
+			$this->load_models, [
+			]
+		);
+		$this->loadModels();
+		$this->controller = mb_strtolower(get_class($this));
+    }
+
+    protected function isPost() {
+        return $_SERVER['REQUEST_METHOD'] === 'POST' ? true : false;
+    }
+
+    protected function build($view) {
+        if ($view) {
+            $this->data['view'] = $view;
+            $this->data['controller'] = $this->controller;
+            $this->data['flash'] = $this->getFlashMessages();
+            $this->data['js_custom_files'] = $this->js_custom_files;
+            $this->data['css_custom_files'] = $this->css_custom_files;
+            $this->load->view('layouts/Default', $this->data);
+        }
+        return false;
+    }
+
+
+    public function _remap($method, $params = array()) {
+		if (method_exists($this, $method)) {
+			return call_user_func_array(array($this, $method), $params);
+		} else {
+			return call_user_func_array(array($this, 'index'), array_merge((array)$method, $params));
+		}
+    }
+
+    protected function loadModels() {
+        foreach ($this->load_models as $model) {
+            $this->load->model($model . "ModelMap");
+            $this->load->model($model . "Model");
+        }
+    }
+
+    protected function getFlashMessages() {
+        $flashArray = $this->session->flashdata();
+        $return = array(
+            'type' => isset($flashArray['type']) ? $flashArray['type'] : false,
+            'title' => isset($flashArray['title']) ? $flashArray['title'] : false,
+            'text' => isset($flashArray['text']) ? $flashArray['text'] : false,
+        );
+        if ($return['type'] != false) {
+            unset($_SESSION['text'], $_SESSION['__ci_vars']['text']);
+        }
+        return $return;
+    }
+}
---
+++ application/core/MasterModel.php
@@ -0,0 +1,130 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MasterModel extends CI_Model {
+
+    protected $db_prefix;
+    protected $class_name;
+    protected $db_tablename;
+    protected $db_fields;
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+        $this->db_prefix = $this->db->dbprefix;
+        $this->class_name = get_class($this);
+    }
+
+    public function getDbTableName() {
+        return $this->db_tablename;
+    }
+
+    public function setDbTableName($db_tablename) {
+        $this->db_tablename = $this->db_prefix . $db_tablename;
+    }
+
+    public function getDbFields() {
+        return $this->db_fields;
+    }
+
+    public function setDbFields($db_fields) {
+        $this->db_fields = $db_fields;
+    }
+
+    public function fromArray($array, $strict = false) {
+        if (is_array($array) && !empty($array)) {
+            foreach ($array as $key => $value) {
+                if (!$strict) {
+                    $this->$key = $value;
+                } else {
+                    if ($this->checkProperty($key))
+                        $this->$key = $value;
+                }
+            }
+        }
+    }
+
+    public function save() {
+        if (!isset($this->id) || !$this->id) {
+            return $this->insert();
+        } else {
+            return $this->update();
+        }
+    }
+
+    public function insert() {
+        $object_array = $this->toArray(true);
+
+        $insert_data = array();
+        foreach ($this->db_fields as $field) {
+            if (isset($object_array[$field])) {
+                $insert_data[$field] = $object_array[$field];
+            }
+
+
+            if (isset($object_array[$field]) && $object_array[$field] === 'NULL') {
+                $insert_data[$field] = NULL;
+            }
+        }
+
+        $this->db->insert($this->db_tablename, $insert_data);
+        $insert_id = $this->db->insert_id();
+        if ($this->checkProperty('id')) {
+            $this->id = $insert_id;
+        }
+        return $insert_id;
+    }
+
+    public function update($pk = false) {
+        $object_array = $this->toArray(true);
+
+        $propery = 'id';
+        if ($pk) {
+            if ($this->checkProperty($pk)) {
+                $propery = $pk;
+            }
+        }
+
+        $update_data = array();
+        foreach ($this->db_fields as $field) {
+            if (isset($object_array[$field])) {
+                $update_data[$field] = $object_array[$field];
+            }
+            if (isset($object_array[$field]) && ($object_array[$field] === 'NULL')) {
+                $update_data[$field] = NULL;
+            }
+        }
+
+        $this->db->where($propery, $this->$propery);
+        $this->db->update($this->db_tablename, $update_data);
+
+        $aff_rows = $this->db->affected_rows();
+        return $aff_rows;
+    }
+
+    public function checkProperty($property, $property_only = false) {
+        if (is_string($property)) {
+            if (!$property_only) {
+                if (property_exists($this, $property) && in_array($property, $this->db_fields))
+                    return true;
+            } else {
+                if (property_exists($this, $property))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public function getById($id) {
+        if ($this->checkProperty('id')) {
+            $object = $this->db->select()
+                ->from($this->db_tablename)
+                ->where([
+                    'id' => $id,
+                ])->get()->first_row(get_class($this));
+            return !empty($object) ? $object : false;
+        }
+        return false;
+    }
+
+}
